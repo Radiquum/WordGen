ad2e4ee93e54429c839264a380be19e2f4be697e
Kentai Radiquum <kentai.waah@gmail.com>
2024-08-28 03:11:00 +0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# WordGen - Word Genetics
+
+Random word generator inspired by [@tsoding](https://github.com/tsoding) [video](https://youtu.be/_rrL20htMAM?si=0AlLlLaJJ3TwFJ3I&t=2884)
+
+## Features
+
+- generation of random words
+- generation strategies
+- liked words
+- mutation of words
+- selection of syllables to include
---
+++ script.js
@@ -0,0 +1,166 @@
+const VOW = "aeiou";
+const CON = "bcdfghjklmnpqrstvwxyz";
+const CHEM_ELEMENTS_NAMES = [
+    "actinium","aluminum","americium","antimony","argon","arsenic","astatine","barium","berkelium","beryllium","bismuth","bohrium","boron","bromine","cadmium","calcium","californium","carbon","cerium","cesium","chlorine","chromium","cobalt","copernicium","copper","curium","darmstadtium","dubnium","dysprosium","einsteinium","erbium","europium","fermium","flerovium","fluorine","francium","gadolinium","gallium","germanium","gold","hafnium","hassium","helium","holmium","hydrogen","indium","iodine","iridium","iron","krypton","lanthanum","lawrencium","lead","lithium","livermorium","lutetium","magnesium","manganese","meitnerium","mendelevium","mercury","molybdenum","moscovium","neodymium","neon","neptunium","nickel","nihonium","niobium","nitrogen","nobelium","oganesson","osmium","oxygen","palladium","phosphorus","platinum","plutonium","polonium","potassium","praseodymium","promethium","protactinium","radium","radon","rhenium","rhodium","roentgenium","rubidium","ruthenium","rutherfordium","samarium","scandium","seaborgium","selenium","silicon","silver","sodium","strontium","sulfur","tantalum","technetium","tellurium","tennessine","terbium","thallium","thorium","thulium","titanium","tungsten","uranium","vanadium","xenon","ytterbium","yttrium","zinc","zirconium"
+]
+const CHEM_ELEMENTS_SYMBOLS = [
+    "ac","al","am","sb","ar","as","at","ba","bk","be","bi","bh","b","br","cd","ca","cf","c","ce","cs","cl","cr","co","cn","cu","cm","ds","db","dy","es","er","eu","fm","fl","f","fr","gd","ga","ge","au","hf","hs","he","ho","h","in","i","ir","fe","kr","la","lr","pb","li","lv","lu","mg","mn","mt","md","hg","mo","mc","nd","ne","np","ni","nh","nb","n","no","og","os","o","pd","p","pt","pu","po","k","pr","pm","pa","ra","rn","re","rh","rg","rb","ru","rf","sm","sc","sg","se","si","ag","na","sr","s","ta","tc","te","ts","tb","tl","th","tm","sn","ti","w","u","v","xe","yb","y","zn","zr"
+]
+
+function get_random_vow() {
+  return VOW[Math.floor(Math.random() * VOW.length)];
+}
+function get_random_con() {
+  return CON[Math.floor(Math.random() * CON.length)];
+}
+function get_random_element_name() {
+    return CHEM_ELEMENTS_NAMES[Math.floor(Math.random() * CHEM_ELEMENTS_NAMES.length)];
+}
+function get_random_element_symbol() {
+    return CHEM_ELEMENTS_SYMBOLS[Math.floor(Math.random() * CHEM_ELEMENTS_SYMBOLS.length)];
+}
+
+let MAX_SYLLABLES = 3;
+let MAX_WORDS = 10;
+
+function change_max_syllables_range() {
+  MAX_SYLLABLES = document.getElementById("MAX_SYLLABLES_range").value;
+  document.getElementById("MAX_SYLLABLES_number").value = MAX_SYLLABLES;
+}
+function change_max_syllables_number() {
+  MAX_SYLLABLES = document.getElementById("MAX_SYLLABLES_number").value;
+  document.getElementById("MAX_SYLLABLES_range").value = MAX_SYLLABLES;
+}
+function change_max_words_range() {
+  MAX_WORDS = document.getElementById("MAX_WORDS_range").value;
+  document.getElementById("MAX_WORDS_number").value = MAX_WORDS;
+}
+function change_max_words_number() {
+  MAX_WORDS = document.getElementById("MAX_WORDS_number").value;
+  document.getElementById("MAX_WORDS_range").value = MAX_WORDS;
+}
+
+change_max_syllables_range();
+change_max_words_range();
+
+if (!localStorage.getItem("liked")) {
+  localStorage.setItem("liked", JSON.stringify([]));
+}
+
+function like(word) {
+  let liked = JSON.parse(localStorage.getItem("liked"));
+  if (liked.includes(word)) return;
+  liked.push(word);
+  localStorage.setItem("liked", JSON.stringify(liked));
+  print_liked();
+}
+
+function dislike(word) {
+  let liked = JSON.parse(localStorage.getItem("liked"));
+  liked = liked.filter((w) => w !== word);
+  localStorage.setItem("liked", JSON.stringify(liked));
+  print_liked();
+}
+
+function print_liked() {
+  let liked = JSON.parse(localStorage.getItem("liked"));
+  document.getElementById("liked").innerHTML = "";
+  liked.forEach((word) => {
+    document.getElementById(
+      "liked"
+    ).innerHTML += `<p>${word} <button onclick="dislike('${word}')">Dislike</button> <button onclick="mutate_char('${word}')">Mutate Char</button> <button onclick="mutate_syl('${word}')">Mutate Syllable</button></p>`;
+  });
+}
+
+print_liked();
+
+function print_random_syllable() {
+  document.getElementById("output").innerHTML = "";
+  const ENABLED_STYLES = [];
+  ["v", "vc", "cv", "cvc", "chem_sym"].forEach((style) => {
+    document.getElementById(style).checked && ENABLED_STYLES.push(style);
+  });
+  if (ENABLED_STYLES.length === 0) {
+    alert("Please select at least one style");
+    return;
+  }
+  for (let i = 0; i < MAX_WORDS; i++) {
+    let word = "";
+    for (let j = 0; j < MAX_SYLLABLES; j++) {
+      const STYLES = {
+        v: `${get_random_vow()}`,
+        vc: `${get_random_vow()}${get_random_con()}`,
+        cv: `${get_random_con()}${get_random_vow()}`,
+        cvc: `${get_random_con()}${get_random_vow()}${get_random_con()}`,
+        chem_sym: `${get_random_element_symbol()}`,
+    };
+      word +=
+        STYLES[
+          ENABLED_STYLES[Math.floor(Math.random() * ENABLED_STYLES.length)]
+        ];
+    }
+    document.getElementById(
+      "output"
+    ).innerHTML += `<p>${word} <button onclick="like('${word}')">Like</button> <button onclick="mutate_char('${word}')">Mutate Char</button> <button onclick="mutate_syl('${word}')">Mutate Syllable</button></p>`;
+  }
+}
+
+function mutate_char(word) {
+  document.getElementById("output").innerHTML = "";
+  for (let i = 0; i < MAX_WORDS; i++) {
+    let new_word = [...word];
+    const pos = Math.floor(Math.random() * new_word.length);
+    const rand_char = word[pos];
+    if (VOW.includes(rand_char)) {
+      new_word[pos] = get_random_vow();
+    } else if (CON.includes(rand_char)) {
+      new_word[pos] = get_random_con();
+    } else {
+      alert("Unable to mutate");
+      return;
+    }
+    new_word = new_word.join("");
+    document.getElementById(
+      "output"
+    ).innerHTML += `<p>${new_word} <button onclick="like('${new_word}')">Like</button> <button onclick="mutate_char('${new_word}')">Mutate Char</button>  <button onclick="mutate_syl('${new_word}')">Mutate Syllable</button></p>`;
+  }
+}
+
+function mutate_syl(word) {
+  document.getElementById("output").innerHTML = "";
+  const ENABLED_STYLES = [];
+  ["v", "vc", "cv", "cvc", "chem_sym"].forEach((style) => {
+    document.getElementById(style).checked && ENABLED_STYLES.push(style);
+  });
+  if (ENABLED_STYLES.length === 0) {
+    alert("Please select at least one style");
+    return;
+  }
+  for (let i = 0; i < MAX_WORDS; i++) {
+    const STYLES = {
+      v: `${get_random_vow()}`,
+      vc: `${get_random_vow()}${get_random_con()}`,
+      cv: `${get_random_con()}${get_random_vow()}`,
+      cvc: `${get_random_con()}${get_random_vow()}${get_random_con()}`,
+      chem_sym: `${get_random_element_symbol()}`,
+    };
+    const syl = Math.floor(Math.random() * 3);
+    const pos = Math.floor(Math.random() * word.length);
+    const word_syl = word.slice(pos, pos + syl + 1);
+    const rand_syl =
+      STYLES[ENABLED_STYLES[Math.floor(Math.random() * ENABLED_STYLES.length)]];
+    const new_word = word.replace(word_syl, rand_syl);
+
+    document.getElementById(
+      "output"
+    ).innerHTML += `<p>${new_word} <button onclick="like('${new_word}')">Like</button> <button onclick="mutate_char('${new_word}')">Mutate Char</button> <button onclick="mutate_syl('${new_word}')">Mutate Syllable</button></p>`;
+  }
+}
+
+function print_random() {
+    if (document.getElementById("random").checked) {
+        print_random_syllable();
+    } else if (document.getElementById("chem_el").checked) {
+        mutate_syl(get_random_element_name())
+    }
+}
